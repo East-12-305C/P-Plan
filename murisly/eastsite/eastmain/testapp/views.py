263dd62ded00d4edba628c8aedbf2d054ce03c13
east305 <east305@iZ253chwpypZ.(none)>
2015-09-27 09:10:26 +0800

    east305 start

---
+++ murisly/eastsite/eastmain/testapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def hello(request):
+	return HttpResponse("hello everyone, this a test east305c!");
