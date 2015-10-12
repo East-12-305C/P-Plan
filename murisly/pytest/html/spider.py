4ee9fa074312cac6428b056170937a7bad35edd0
chrosyou <646680299@qq.com>
2015-10-12 11:41:34 +0800

    url_catch

--- murisly/pytest/html/spider.py
+++ murisly/pytest/html/spider.py
@@ -3,7 +3,9 @@
 import requests
 from lxml import etree
 import getonepage
+import url_catch_queue
 
+url_catch_queue.insert_into_database("");
 
 cook = {"Cookie": "_T_WM=48ec7041fc543364f6424622a3992524; SUB=_2A257Dz7iDeTxGeRG7VYW9i_NzTiIHXVY8EKqrDV6PUJbrdANLUPdkW0Dpvzy7xSdMTg8Y4Zo95O42YNPgQ..; gsid_CTandWM=4uoB5e0911Ct5h8dFhSw5c1fP2E"};
 url = 'http://weibo.cn/u/1931534830' #
@@ -11,8 +13,9 @@ url = 'http://weibo.cn/u/1931534830' #
 # html = requests.get(url).content
 
 
-
+'''
 html = requests.get(url, cookies = cook).content   #get byte
+'''
 #print(chardet.detect(html))  #detect code way
 #html = requests.get(url, cookies = cook).text   #get str
 
@@ -20,13 +23,13 @@ html = requests.get(url, cookies = cook).content   #get byte
 #change code way
 #html = bytes(bytearray(html, encoding='utf-8'))
 
-weibovalue = getonepage.getWeiboContext(html);
-for element in weibovalue:
-    print(element);
+#weibovalue = getonepage.getWeiboContext(html);
+#for element in weibovalue:
+#    print(element);
 
-context = getonepage.getWeiboUrl(html);
-for element in context:
-    print(element);
+#context = getonepage.getWeiboUrl(html);
+#for element in context:
+#    print(element);
 
 
 
