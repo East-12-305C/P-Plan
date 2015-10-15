836231bcab34eda26dab5c32349df1863bd26679
chrosyou <646680299@qq.com>
2015-10-15 00:16:08 -0700

    mysql query

--- murisly/eastsite/eastmain/testapp/views.py
+++ murisly/eastsite/eastmain/testapp/views.py
@@ -5,9 +5,15 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 
 def hello(request):
-    return HttpResponse("hello everyone, this a test east305c! write by python!");
+    return HttpResponse("everyone, this a test east305c! write by python!");
 
 def home(request):
     TutorialList = ["HTML", "CSS", "jQuery", "Python", "Django"]
     welfunc = "this is a conbine!";
     return render(request, 'home.html', {"namelist": TutorialList});
+
+def weibotest(request):
+    a = request.GET.get("name");
+    if a is None:
+        return render(request, 'home.html');
+    return HttpResponse("the is:" + str(a));
