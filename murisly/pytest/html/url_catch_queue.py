b4cd386409f892758f755cc54812df8249820c26
chrosyou <646680299@qq.com>
2015-10-21 00:13:06 +0800

    mysql change

--- murisly/pytest/html/url_catch_queue.py
+++ murisly/pytest/html/url_catch_queue.py
@@ -16,52 +16,55 @@ import time
 
 class sqlOperate():
     def __init__(self):
-        self.connect = None;
-        self.cursor =None;
+        self.connect = None
+        self.cursor =None
 
-        self.url_alluser     = "alluser";
-        self.url_firstuser   = "firstuser";
-        self.url_unvisituser = "unvisituser";
+        self.url_alluser     = "alluser"
+        self.url_firstuser   = "firstuser"
+        self.url_unvisituser = "unvisituser"
 
         try:
-            self.connect = pymysql.connect(host='localhost',user='root',passwd='east',db='spider',port=3306)
-            self.cursor = self.connect.cursor();
+            self.connect = pymysql.connect(host='localhost',user='root',passwd='east',db='spider',port=3306, charset="gbk")
+            self.cursor = self.connect.cursor()
 
-            self.cursor.execute("show tables;");
-            tables = self.cursor.fetchall();
+            self.cursor.execute("show tables;")
+            tables = self.cursor.fetchall()
 
-            ut_firstuser = False;
-            ut_alluser = False;
-            ut_unvisituser = False;
+            ut_firstuser = False
+            ut_alluser = False
+            ut_unvisituser = False
             for element in tables:
                 if element[0] == self.url_firstuser:
-                    ut_firstuser = True;
+                    ut_firstuser = True
                 elif element[0] == self.url_alluser:
-                    ut_alluser = True;
+                    ut_alluser = True
                 elif element[0] == self.url_unvisituser:
-                    ut_unvisituser = True;
+                    ut_unvisituser = True
 
             if False == ut_firstuser:
                 print("create firstuser table...")
-                self.cursor.execute("create table %s(id int(10) not null auto_increment, nickname char(32), sex tinyint(2), address char(10), birthday int(8), weibos int(6), follow int(9), interes int(5), PRIMARY KEY(id))" % self.url_firstuser);
+                self.cursor.execute("create table %s(id bigint(10) not null auto_increment, nickname char(32), sex tinyint(2), address char(10), birthday int(8), weibos int(6), follow int(9), interes int(5), PRIMARY KEY(id))" % self.url_firstuser);
+                self.cursor.execute("alter table %s convert to charset gbk;" % (self.url_firstuser));
                 print("create firstuser table success...")
 
             if False == ut_alluser:
                 print("create alluser table...")
-                self.cursor.execute("create table %s(id int(10) not null auto_increment, nickname char(32) not null, PRIMARY KEY(id))" % self.url_alluser);
+                self.cursor.execute("create table %s(id bigint(10) not null auto_increment, nickname char(32) not null, PRIMARY KEY(id))" % self.url_alluser);
+                self.cursor.execute("alter table %s convert to charset gbk;" % (self.url_alluser));
                 print("create alluser table success...")
 
             if False == ut_unvisituser:
                 print("create unvisituser table...")
-                self.cursor.execute("create table %s(id int(10) not null auto_increment, PRIMARY KEY(id))" % self.url_unvisituser);
+                self.cursor.execute("create table %s(id bigint(10) not null auto_increment, PRIMARY KEY(id))" % self.url_unvisituser);
+                self.cursor.execute("alter table %s convert to charset gbk;" % (self.url_unvisituser));
                 print("create unvisituser table success...")
         except Exception:
-            errormode.errorWriting(__file__ + "  sqlOperate");
+            errormode.errorWriting(__file__ + "  sqlOperate")
 
 
     def __del__(self):
-        self.cursor.close();
-        self.connect.close();
+        self.cursor.close()
+        self.connect.close()
 
 
     def getUnvisit(self, inum):
@@ -70,15 +73,15 @@ class sqlOperate():
         :param num: 取出未访问的用户个数
         :return: 返回未访问用户的表
         '''
-        userids = [];
+        userids = []
         try:
             self.cursor.execute("select * from %s limit %d;" % (self.url_unvisituser, inum));
-            ret = self.cursor.fetchall();
+            ret = self.cursor.fetchall()
             if len(ret) > 0 :
-                userids = ret[0];
+                userids = ret[0]
             self.cursor.execute("delete from %s limit %d;" % (self.url_unvisituser, inum));
         except Exception:
-            errormode.errorWriting(__file__ + "  getUnvisit");
+            errormode.errorWriting(__file__ + "  getUnvisit")
 
         return userids;
 
@@ -115,24 +118,29 @@ class sqlOperate():
 
     def updateAlluser(self, userid, nickname):
         try:
-            self.cursor.execute("INSERT allusers values (%d, %s) ON DUPLICATE KEY UPDATE nickname = %s;" % (int(userid), nickname, nickname));
+            sql = "insert alluser values(%d, '%s') ON DUPLICATE KEY UPDATE nickname = '%s';" % (int(userid), nickname, nickname)
+            print(sql)
+            self.cursor.execute(sql);
             self.connect.commit();
         except Exception:
             errormode.errorWriting(__file__ + "  updateAlluser");
 
     def updateFirstuser(self, userinfo):
+
+        id = userinfo["id"];
+        nickname = userinfo["nickname"];
+        sex = userinfo["sex"];
+        address = userinfo["address"];
+        birthday = userinfo["birthday"];
+        weibos = userinfo["weibos"];
+        follow = userinfo["follow"];
+        interes = userinfo["interes"];
+        sql = 'insert firstuser values(%s,"%s",%s,"%s",%s,%s,%s,%s) ON DUPLICATE KEY UPDATE nickname="%s",sex=%s,address="%s",birthday=%s,weibos=%s,follow=%s,interes=%s;' % (id,nickname,sex,address,birthday,weibos,follow,interes, nickname,sex,address,birthday,weibos,follow,interes);
+        print(sql);
+        self.cursor.execute(sql);
+        self.connect.commit();
         try:
-            id = userinfo["id"];
-            nickname = userinfo["nickname"];
-            sex = userinfo["sex"];
-            address = userinfo["address"];
-            birthday = userinfo["birthday"];
-            weibos = userinfo["weibos"];
-            follow = userinfo["follow"];
-            interes = userinfo["interes"];
-            a = 'INSERT firstuser values (%s,"%s",%s,"%s",%s,%s,%s,%s) ON DUPLICATE KEY UPDATE nickname="%s",sex=%s,address="%s",birthday=%s,weibos=%s,follow=%s,interes=%s;' % (id,nickname,sex,address,birthday,weibos,follow,interes, nickname,sex,address,birthday,weibos,follow,interes);
-            self.cursor.execute(a);
-            self.connect.commit();
+            a = 1;
         except Exception:
             errormode.errorWriting(__file__ + "  updateFirstuser");
 
