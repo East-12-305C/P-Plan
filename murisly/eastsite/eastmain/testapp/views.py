277c3fb812463ce158a2415aab1e5cc1be4b7248
chrosyou <646680299@qq.com>
2015-10-28 22:29:08 +0800

    test1

--- murisly/eastsite/eastmain/testapp/views.py
+++ murisly/eastsite/eastmain/testapp/views.py
@@ -4,13 +4,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from testapp.mysqlhelper import *
-import mysqlhelper
+
 
 def hello(request):
     return HttpResponse("everyone, this a test east305c! write by python!");
 
 def home(request):
-    weiboldict = mysqlhelper.getweibototal();
+    weiboldict = getweibototal();
     welfunc = "this is a conbine!";
     return render(request, 'home.html', {"namelist": TutorialList});
 
