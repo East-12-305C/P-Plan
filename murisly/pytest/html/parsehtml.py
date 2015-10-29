3606d33b723664204bc0e2e86048c33611f1862f
chrosyou <646680299@qq.com>
2015-10-29 17:37:14 +0800

    test1

--- murisly/pytest/html/parsehtml.py
+++ murisly/pytest/html/parsehtml.py
@@ -26,7 +26,7 @@ class Cookies():
             #tezhong69239@163.com
             #{"Cookie": "_T_WM=7267c27f388b035051df7cbf9ca6bc99; SUB=_2A257L8R2DeTxGeNP6VUQ-SbEzT2IHXVY0-w-rDV6PUJbvNANLWrbkW1TSpJsY6WEsn_TN2wEVGfAPnBv3w..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWL6A.u2VkuC0YVxrvvdvhd5JpX5K-t; SUHB=0M2Vo3Kp6M1IT0; SSOLoginState=1445704742"},
             #xie814403@163.com
-            {"Cookie": "_T_WM=9c995b353baf09efb0dd45642f0f9bf4; SUB=_2A257NZc8DeTxGeNP6VUV9S3KzjyIHXVY2Tl0rDV6PUJbvNAKLRbRkW1edjAHbO3El-y1eufaAmQlE4HdKA..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhEVnZoJjDdlcUYh6iPgBve5JpX5K-t; SUHB=0nl-NdMXIlJsPS; SSOLoginState=1446111084"},
+            {"Cookie": "_T_WM=9c995b353baf09efb0dd45642f0f9bf4; SUHB=0nl-NdMXIlJsPS; SUB=_2A257NZj8DeTxGeRG7VYW9i_NzTiIHXVY2Ti0rDV6PUJbrdAKLXn1kW2KraJvoarfW_xaVB9FbIAbg0VwUQ..; gsid_CTandWM=4u15fccf1ycdqnJTQDwzuc1fP2E"},
         )
 
         self.pos = 0;
